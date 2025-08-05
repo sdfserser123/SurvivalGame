9df146a4a4a17171523a6db00cb87aed56fa5bbb
sdfserser123 <sdfserser1234@gmail.com>
2025-08-05 08:48:41 +0700

    feat: Added Placeable and Breakable object

--- Scripts/Objects/placeable_checker.gd
+++ Scripts/Objects/placeable_checker.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var parent = self.get_parent()
 @onready var sprite = parent.get_node("WorkbenchTexture")
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var available = true
 var original_color
@@ -16,12 +17,14 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Buildings"):
+	print("Bắt đầu va chạm với:", body.name)
+	if body.is_in_group("Buildings") || body.name == "TileMapLayer":
 		sprite.modulate = Color(0.8, 0, 0, 0.5)
 		available = false
 	else:
 		sprite.modulate = original_color
 		available = true
+
 
 
 func _on_body_exited(body: Node2D) -> void:
